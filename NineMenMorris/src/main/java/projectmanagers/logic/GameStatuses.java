fb2c2662638d420f39c2a94946863107d1d35088
Nathanael G <47373466+StrainedQuacking@users.noreply.github.com>
2019-09-13 15:21:09 -0500

    removed unnecessary import

--- NineMenMorris/src/main/java/projectmanagers/logic/GameStatuses.java
+++ NineMenMorris/src/main/java/projectmanagers/logic/GameStatuses.java
@@ -1,7 +1,5 @@
 package projectmanagers.logic;
 
-import java.util.HashMap;
-
 public class GameStatuses {
-    enum ColorStatus {EMPTY, BLACK, WHITE}
+    public enum ColorStatus {EMPTY, BLACK, WHITE}
 }
